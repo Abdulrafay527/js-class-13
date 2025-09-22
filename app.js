c7af5f9604feea6d4fcee878a0728eaf8fd20106
Abdulrafay527 <rafaymoosait@gmail.com>
2025-09-22 15:28:56 +0500

    js-class-13 completed

---
+++ app.js
@@ -0,0 +1,29 @@
+// var str = "SMIT Sayalni";
+
+// console.log(str.lastIndexOf("M"))
+
+var arr = ['ghous' , 'ahmed' , 'ali']
+
+console.log(arr.indexOf("ali"))
+
+
+
+
+
+// var str = "Sayalni SMIT SMIT Sas SMIT ada SMIT";
+// var word = "SMIT";
+// var rep = "Sayalni";
+// var khali = "";
+// var count = 0;
+
+// for(var i = 0; i < str.length; i++){
+//     var find = str.slice(i, i + word.length);
+//     if(find === word){
+//         count++;
+//        if(count === 3){
+//         str = str.slice(0, i) + rep + str.slice(i + word.length)
+//        }
+//     }
+// }
+
+// console.log(str)
